c28ef0a02fe941d7a506c4a5ceb19251bff46d29
yangguanghaitan <2898666969@qq.com>
2022-01-18 23:33:01 +0800

    AQS之CyclicBarrier#CyclicBarrier(int, java.lang.Runnable)示例

---
+++ src/main/java/guide/concurrent/AQS/CyclicBarrierExample3.java
@@ -0,0 +1,49 @@
+package guide.concurrent.AQS;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Auther d
+ * @Date 2022/1/18 23:28
+ * @Describe 本例测试 {@link CyclicBarrier#CyclicBarrier(int, java.lang.Runnable)}
+ **/
+
+public class CyclicBarrierExample3 {
+    // 请求的数量
+    private static final int threadCount = 6;
+    // 需要同步的线程数量
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(5, () -> {
+        System.out.println("------当线程数达到之后，优先执行------");
+    });
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            threadPool.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+
+    public static void test(int threadnum) throws InterruptedException, BrokenBarrierException {
+        System.out.println("threadnum:" + threadnum + "is ready");
+        cyclicBarrier.await();
+        System.out.println("threadnum:" + threadnum + "is finish");
+    }
+
+}
+
