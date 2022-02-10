ddda3f2164a6f5ed8725a4a81cd797f52e1f33f8
yangguanghaitan <2898666969@qq.com>
2022-02-10 20:18:52 +0800

    设计模式之observe(java的event listener)观察者模式

---
+++ src/main/java/patterns/observe/event/Student.java
@@ -0,0 +1,20 @@
+package patterns.observe.event;
+
+/**
+ * @Auther d
+ * @Date 2022/2/10 19:54
+ * @Describe 具体的监听器，类似于观察者
+ **/
+public class Student implements HomeworkEventListen{
+    private String name;
+    public Student(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void update(HomeworkEventObject o, Object arg) {
+        Teacher teacher = o.getTeacher();
+        System.out.printf("学生%s观察到（实际是被通知）%s布置了作业《%s》 \n",
+                this.name, teacher.getName(), arg);
+    }
+}
