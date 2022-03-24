b908edf8752b924f21c9eac7841fad7d3d161499
yangguanghaitan <2898666969@qq.com>
2022-03-24 09:55:09 +0800

    不重要

--- src/main/java/log/HelloWorld.java
+++ src/main/java/log/HelloWorld.java
@@ -3,20 +3,22 @@ package log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.PrintStream;
-
 public class HelloWorld {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(HelloWorld.class);
         final Logger sys_user_logger = LoggerFactory.getLogger("sys-user");
-        logger.info("Hello World");
-        logger.error("22222222222");
+        logger.trace("=====trace=====");
+        logger.debug("=====debug=====");
+        logger.info("=====info=====");
+        logger.warn("=====warn=====");
+        logger.error("=====error=====");
         sys_user_logger.error("5555555");
 //        logger.atInfo().addArgument(() -> t16()).log( "Temperature set to {}. Old temperature was {}.", 99);
 //        logger.atDebug().addArgument(4).addArgument(7).log( "Temperature set to {}. Old temperature was {}.");
 
     }
-    public static int t16(){
+
+    public static int t16() {
         return 90;
     }
 }
