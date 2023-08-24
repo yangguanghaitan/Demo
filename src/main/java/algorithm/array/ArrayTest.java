7fd7e0a834271dce9a9da68073321ae6b9a870ea
2898666969@qq.com <2898666969@qq.com>
2023-08-24 18:45:32 +0800

    不重要

---
+++ src/main/java/algorithm/array/ArrayTest.java
@@ -0,0 +1,20 @@
+package algorithm.array;
+
+import org.junit.Test;
+
+/**
+ * @Auther d
+ * @Date 2023/8/24 18:26
+ * @Describe
+ **/
+
+public class ArrayTest {
+
+
+    @Test
+    public void test01(){
+        int[][] aa={{0, 1, 2},{3, 4, 5}};
+        System.out.println(aa[1][1]);
+        System.out.println(aa);
+    }
+}
---
+++ src/main/java/algorithm/array/BinarySearch.java
@@ -0,0 +1,19 @@
+package algorithm.array;
+
+import org.junit.Test;
+
+/**
+ * @Auther d
+ * @Date 2023/8/24 18:35
+ * @Describe 二分查找
+ **/
+public class BinarySearch {
+
+    int nums[] = {-1, 0, 3, 5, 9, 12};
+
+
+    @Test
+    public void BS01() {
+
+    }
+}
